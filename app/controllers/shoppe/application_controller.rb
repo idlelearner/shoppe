fdec73b891eb1f179580ee6513debf3057fbe76c
RenYuan <renyuan.zou@icloud.com>
2015-07-18 18:21:12 +0200

    change ruby version and fix method name error

--- app/controllers/shoppe/application_controller.rb
+++ app/controllers/shoppe/application_controller.rb
@@ -1,12 +1,12 @@
 module Shoppe
   class ApplicationController < ActionController::Base
-    
+
     before_filter :login_required
-    
+
     rescue_from ActiveRecord::DeleteRestrictionError do |e|
       redirect_to request.referer || root_path, :alert => e.message
     end
-    
+
     rescue_from Shoppe::Error do |e|
       @exception = e
       render :layout => 'shoppe/sub', :template => 'shoppe/shared/error'
@@ -23,9 +23,9 @@ module Shoppe
     def logged_in?
       current_user.is_a?(User)
     end
-    
+
     def current_user
-      @current_user ||= login_from_session || login_with_demo_mdoe || :false
+      @current_user ||= login_from_session || login_with_demo_mode || :false
     end
 
     def login_from_session
@@ -33,14 +33,14 @@ module Shoppe
         @user = User.find_by_id(session[:shoppe_user_id])
       end
     end
-    
-    def login_with_demo_mdoe
+
+    def login_with_demo_mode
       if Shoppe.settings.demo_mode?
         @user = User.first
       end
     end
-    
+
     helper_method :current_user, :logged_in?
-    
+
   end
 end
